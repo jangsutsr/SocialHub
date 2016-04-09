ecc20e5c2a011fa31b2f596d409aedb2ced0eee3
jangsutsr <jangsutsr@gmail.com>
2016-04-09 14:59:49 -0400

    initial schema

--- SocialHub/main_app/models.py
+++ SocialHub/main_app/models.py
@@ -3,3 +3,39 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class UserRecord(models.Model):
+    category = models.IntegerField()
+    name = models.CharField(max_length=30, default='')
+    passwd = models.CharField(max_length=30, default='')
+    class Meta(object):
+        db_table = 'user_record'
+        unique_together = (
+            ('name', ),
+        )
+
+class User(models.Model):
+    name = models.CharField(max_length=30, default='')
+    passwd = models.CharField(max_length=30, default='')
+    twitter = models.ForeignKey('UserRecord', on_delete=models.CASCADE, related_name='+', null=True)
+    facebook = models.ForeignKey('UserRecord', on_delete=models.CASCADE, related_name='+', null=True)
+    class Meta(object):
+        db_table = 'user'
+        unique_together = (
+            ('name', ),
+        )
+
+class FacebookMsg(models.Model):
+    content = models.TextField(default='')
+    time = models.DateTimeField(null=True)
+    loc = models.CharField(max_length=30, default='')
+    owner = models.ForeignKey('UserRecord', on_delete=models.CASCADE, null=True)
+    class Meta(object):
+        db_table = 'facebook_msg'
+
+class TwitterMsg(models.Model):
+    content = models.TextField(default='')
+    time = models.DateTimeField(null=True)
+    loc = models.CharField(max_length=30, default='')
+    owner = models.ForeignKey('UserRecord', on_delete=models.CASCADE, null=True)
+    class Meta(object):
+        db_table = 'twitter_msg'
