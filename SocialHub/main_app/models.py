8b78e9d571f43b8901d476142be3d4771b93d8ca
jangsutsr <jangsutsr@gmail.com>
2016-04-17 14:13:52 -0400

    modify

--- SocialHub/main_app/models.py
+++ SocialHub/main_app/models.py
@@ -2,40 +2,50 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
-class UserRecord(models.Model):
-    category = models.IntegerField()
-    name = models.CharField(max_length=30, default='')
-    passwd = models.CharField(max_length=30, default='')
-    class Meta(object):
-        db_table = 'user_record'
-        unique_together = (
-            ('name', ),
-        )
-
 class User(models.Model):
-    name = models.CharField(max_length=30, default='')
-    passwd = models.CharField(max_length=30, default='')
-    twitter = models.ForeignKey('UserRecord', on_delete=models.CASCADE, related_name='+', null=True)
-    facebook = models.ForeignKey('UserRecord', on_delete=models.CASCADE, related_name='+', null=True)
+    name = models.CharField(max_length=30,
+                            default='')
+    passwd = models.CharField(max_length=30,
+                              default='')
+    twitter = models.ForeignKey('UserRecord',
+                                related_name='+',
+                                null=True)
+    facebook = models.ForeignKey('UserRecord',
+                                 related_name='+',
+                                 null=True)
     class Meta(object):
         db_table = 'user'
         unique_together = (
             ('name', ),
         )
 
+class UserRecord(models.Model):
+    category = models.IntegerField()
+    name = models.CharField(max_length=30,
+                            default='')
+    passwd = models.CharField(max_length=30,
+                              default='')
+    class Meta(object):
+        db_table = 'user_record'
+        unique_together = (
+            ('name', ),
+        )
+
 class FacebookMsg(models.Model):
     content = models.TextField(default='')
-    time = models.DateTimeField(null=True)
+    time = models.DateTimeField(default=None)
     loc = models.CharField(max_length=30, default='')
-    owner = models.ForeignKey('UserRecord', on_delete=models.CASCADE, null=True)
+    owner = models.ForeignKey('UserRecord',
+                              default=None)
     class Meta(object):
         db_table = 'facebook_msg'
 
 class TwitterMsg(models.Model):
     content = models.TextField(default='')
-    time = models.DateTimeField(null=True)
-    loc = models.CharField(max_length=30, default='')
-    owner = models.ForeignKey('UserRecord', on_delete=models.CASCADE, null=True)
+    time = models.DateTimeField(default=None)
+    loc = models.CharField(max_length=30,
+                           default='')
+    owner = models.ForeignKey('UserRecord',
+                              default=None)
     class Meta(object):
         db_table = 'twitter_msg'
