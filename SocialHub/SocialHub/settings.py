c085ce62ae5f5e7eb6cd9c88b6f08d9c94a9355f
jangsutsr <jangsutsr@gmail.com>
2016-05-03 17:48:02 -0400

    Change for MySQL

--- SocialHub/SocialHub/settings.py
+++ SocialHub/SocialHub/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'SocialHub.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': os.path.join(BASE_DIR, 'db_user.cnf'),
+        }
     }
 }
 
