0665c5db15835fc1339a3f50e0584ed7e4851ac6
jangsutsr <jangsutsr@gmail.com>
2016-05-11 12:53:44 -0400

    minor modification of middleware

--- SocialHub/SocialHub/middlewares.py
+++ SocialHub/SocialHub/middlewares.py
@@ -2,4 +2,6 @@ class CorsMiddleware(object):
     def process_response(self, request, response):
         response['Access-Control-Allow-Credentials'] = 'true'
         response['Access-Control-Allow-Origin'] = 'http://0.0.0.0:8100'
+        if response['Content-Type'] == 'text/html; charset=utf-8':
+            response['Content-Type'] = 'text/plain; charset=utf-8'
         return response
