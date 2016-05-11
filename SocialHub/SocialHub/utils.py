5d4b357a71aa72be92933f4ddec2a25925cb9182
jangsutsr <jangsutsr@gmail.com>
2016-05-10 23:27:48 -0400

    Change util function

--- SocialHub/SocialHub/utils.py
+++ SocialHub/SocialHub/utils.py
@@ -1,7 +1,7 @@
 from subprocess import Popen, PIPE
 
 def wav_to_mp3(data):
-    return Popen('~/Downloads/SnowLeopard_Lion_Mountain_Lion_Mavericks_Yosemite_El-Captain_02.05.2016/ffmpeg -i pipe:0 -f mp3 pipe:1',
+    return Popen('/usr/bin/ffmpeg -i pipe:0 -f mp3 pipe:1',
                  stdin=PIPE,
                  stdout=PIPE,
                  shell=True).communicate(data)[0]
